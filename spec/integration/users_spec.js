672fde785a8846d3087d95842cf9f77c99bc84ea
vdowsett <vdowsett@gmail.com>
2019-02-06 17:13:29 -0600

    signup model, controller, routes and views - tested

---
+++ spec/integration/users_spec.js
@@ -0,0 +1,91 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/users/";
+const User = require("../../src/db/models").User;
+const sequelize = require("../../src/db/models/index").sequelize;
+
+describe("routes : users", () => {
+
+  beforeEach((done) => {
+
+    sequelize.sync({force: true})
+    .then(() => {
+      done();
+    })
+    .catch((err) => {
+      console.log(err);
+      done();
+    });
+
+  });
+
+  describe("GET /users/sign_up", () => {
+
+    it("should render a view with a sign up form", (done) => {
+      request.get(`${base}sign_up`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("Sign up");
+        done();
+      });
+    });
+
+  });
+
+  describe("POST /users/create", () => {
+
+    it("should create a new user", (done) => {
+
+        const options = {
+            url: base,
+            form: {
+                email: "user@example.com",
+                password: "password"
+            }
+        }
+
+        request.post(options, (err, res, body) => {
+
+            User.findOne({where: {email: "user@example.com"}})
+            .then((user) => {
+                expect(user).not.toBeNull();
+                expect(user.email).toBe("user@example.com");
+                expect(user.id).toBe(1);
+                done();
+            })
+            .catch((err) => {
+                console.log(err);
+                done();
+            })
+          });
+    });
+
+    it("should not create a user without valid attributes", () => {
+        
+        request.post(
+            {
+                url: base,
+                form: {
+                    email: "not valid",
+                    password: "password"
+                }
+            },
+            (err, req, body) => {
+                
+                User.findOne({where: {email: "not valid"}})
+                .then((user) => {
+                    expect(user).toBeNull();
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+            })
+    })
+  });
+
+//   describe("GET /users/signIn", () => { });
+
+//   describe("GET /users/signOut", () => { });
+  
+//   describe("GET /users/:id", () => { });
+});
