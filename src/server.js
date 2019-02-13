b261d8b579b3f1617df456c5e4a14b7ff6aea88d
vdowsett <vdowsett@gmail.com>
2019-02-13 16:29:06 -0600

    trying to fix heroku

--- src/server.js
+++ src/server.js
@@ -1,24 +1,19 @@
-const app = require("./app.js");
-const http = require("http");
+const app = require( "./app.js" );
+const http = require( "http" );
 
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+const port = normalizePort( process.env.PORT || "3000" );
+app.set( "port", port );
 
-const server = http.createServer(app);
+const server = http.createServer( app );
+server.listen( port, () => {
+  console.log( "server listening on port %O", server.address().port );
+} );
 
-server.listen(port);
+function normalizePort( value ) {
+  const port = parseInt( value, 10 );
+  if ( isNaN( port ) ) { return value; }
+  if ( port >= 0 ) { return port; }
+  return false;
+};
 
-function normalizePort(val) {
-    const port = parseInt(val, 10);
-    if (isNaN(port)) {
-      return val;
-    }
-    if (port >= 0) {
-      return port;
-    }
-    return false;
-  }
-
-server.on("listening", () => {
-    console.log(`server is listening for requests on port ${server.address().port}`);
-});
+module.exports = server;
