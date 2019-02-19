fa24b552686d704eb06442558f6f92df18d9d367
vdowsett <vdowsett@gmail.com>
2019-02-19 09:20:42 -0600

    hiding private wikis to other users in wiki view

--- spec/integration/wikis_spec.js
+++ spec/integration/wikis_spec.js
@@ -60,7 +60,7 @@ describe("Wiki", () => {
 
   });
 
-  describe("wikis user access", () => {
+  describe("wikis regular user interaction with application", () => {
 
     beforeEach((done) => {
       User.create({
@@ -141,6 +141,7 @@ describe("Wiki", () => {
     });
 
     describe("POST /wikis/:id/destroy", () => {
+
       it("should delete the wiki", (done) => {
         expect(this.wiki.id).toBe(1);
         request.post(`${base}/${this.wiki.id}/destroy`, (err, res, body) => {
@@ -155,6 +156,7 @@ describe("Wiki", () => {
     });
 
     describe("GET /wikis/:id/edit", () => {
+
       it("should render an edit view with an update form", (done) => {
         request.get(`${base}/${this.wiki.id}/edit`, (err, res, body) => {
           expect(err).toBeNull();
@@ -165,6 +167,7 @@ describe("Wiki", () => {
     });
 
     describe("POST /wikis/:id/update", () => {
+
       it("should update the wiki with the given values", (done) => {
         const options = {
           url: `${base}${this.wiki.id}/update`,
@@ -181,6 +184,181 @@ describe("Wiki", () => {
           })
           .then((wiki) => {
             expect(wiki.title).toBe("Wiki Editing Example");
+            done();
+          });
+        });
+      });
+    });
+
+  });
+
+  describe("wikis premium user interaction with application", () => {
+
+    beforeEach((done) => {
+      User.create({
+        email: "premium@premium.com",
+        username: "premium",
+        role: 1,
+        password: "12345",
+      })
+      .then((user) => {
+        this.user = user;
+        request.get({
+          url: "http://localhost:3000/auth/fake",
+          form: {
+            email: "premium@premium.com",
+            userId: this.user.id
+          }
+        });
+        done();
+      });
+    });
+
+    describe("GET /wikis", () => {
+        
+      it("should view wiki list with new wiki button visible", (done) => {
+  
+          request.get(base, (err, res, body) => {
+              expect(this.user.id).toBe(2);
+              expect(res.statusCode).toBe(200);
+              expect(err).toBeNull();
+              expect(body).toContain("Wikis");
+              done();
+          });
+          
+      });
+    });
+
+    describe("GET /new", () => {
+        
+      it("should view new wiki page", (done) => {
+  
+        request.get(`${base}new`, (err, res, body) => {
+          expect(res.statusCode).toBe(200);
+          expect(err).toBeNull();
+          expect(body).toContain("New Wiki");
+          done();
+      });
+          
+      });
+    });
+  
+    describe("POST /wikis/create", () => {
+  
+        it("should create a new wiki with associated user ID", (done) => {
+  
+          const options = {
+            url: `${base}create`,
+            form: {
+              title: "Example of wiki premium title",
+              body: "Example of wiki premium body",
+              private: false,
+            }
+          }
+  
+          request.post(options, (err, res, body) => {
+            Wiki.findOne({where: {title: "Example of wiki premium title"}})
+            .then((wiki) => {
+              expect(wiki.title).toBe("Example of wiki premium title");
+              expect(wiki.body).toContain("wiki premium body");
+              expect(wiki.userId).not.toBeNull();
+              done();
+            })
+            .catch((err) => {
+              console.log(err);
+              done();
+            });
+          });
+        });
+
+        it("should create a new wiki and set post to private", (done) => {
+  
+          const options = {
+            url: `${base}create`,
+            form: {
+              title: "Private wiki title",
+              body: "Private wiki body",
+              private: true,
+            }
+          }
+  
+          request.post(options, (err, res, body) => {
+            Wiki.findOne({where: {title: "Private wiki title"}})
+            .then((wiki) => {
+              expect(wiki.title).toBe("Private wiki title");
+              expect(wiki.body).toContain("Private wiki body");
+              expect(wiki.private).toBe(true);
+              expect(wiki.userId).not.toBeNull();
+              done();
+            })
+            .catch((err) => {
+              console.log(err);
+              done();
+            });
+          });
+        });
+
+        describe("GET /wikis", () => {
+        
+          it("should view wiki list but not include the private wiki", (done) => {
+      
+              request.get(base, (err, res, body) => {
+                  expect(res.statusCode).toBe(200);
+                  expect(err).toBeNull();
+                  expect(body).toContain("Wikis");
+                  expect(body).not.toContain("Private wiki title");
+                  done();
+              });
+              
+          });
+        });
+  
+    });
+
+    describe("POST /wikis/:id/destroy", () => {
+
+      it("should delete the wiki", (done) => {
+        expect(this.wiki.id).toBe(1);
+        request.post(`${base}/${this.wiki.id}/destroy`, (err, res, body) => {
+          Wiki.findById(1)
+          .then((wiki) => {
+            expect(err).toBeNull();
+            expect(wiki).not.toBeNull();
+            done();
+          })
+        });
+      });
+    });
+
+    describe("GET /wikis/:id/edit", () => {
+
+      it("should render an edit view with an update form", (done) => {
+        request.get(`${base}/${this.wiki.id}/edit`, (err, res, body) => {
+          expect(err).toBeNull();
+          expect(body).not.toContain("Edit Wiki");
+          done();
+        });
+      });
+    });
+
+    describe("POST /wikis/:id/update", () => {
+      
+      it("should update the wiki with the given values", (done) => {
+        const options = {
+          url: `${base}${this.wiki.id}/update`,
+          form: {
+            title: "Wiki Editing premium Example",
+            body: "changing wiki",
+            private: true,
+            userId: this.user.id
+          }
+        };
+        request.post(options, (err, res, body) => {
+          Wiki.findOne({
+            where: {id: this.wiki.id}
+          })
+          .then((wiki) => {
+            expect(wiki.title).toBe("Wiki Editing premium Example");
             done();
           });
         });
