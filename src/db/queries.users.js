945a789e0069a44874a479ae39daeb6786b8ddce
vdowsett <vdowsett@gmail.com>
2019-02-19 18:39:44 -0600

    brainstorming error

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,5 +1,6 @@
 const User = require("./models").User;
 const Wiki = require("./models").Wiki;
+const wikiQueries = require("../db/queries.wikis.js");
 
 const bcrypt = require("bcryptjs");
 
@@ -74,26 +75,41 @@ module.exports = {
 
   downgradeUser(id, callback) {
 
-    return User.findById(id)
-    
-       .then((user) => {
+    return User.findById(id, {
+      include: [{
+        model: Wiki,
+        as: "wikis",
+        where: {
+          private: true
+        }
+      }]
+    })
+    .then((user) => {
 
-          if(!user){
-            return callback("User not found");
-          }
+      if(!user){
+        return callback("User not found");
+      }
+      
+      user.update( { role: 0 } )
 
-          user.update({ role: 0 })
+      .then((res) => {
+
+        res.wikis.forEach((wiki) => { 
+          wiki.update({private:false})
           .then((res) => {
-            callback(null, user);
+            callback(null, res);
           })
-          .catch((err) => {
-            callback(err);
-          });
-
+          .catch((err) => { 
+            callback(err)
+          })
         })
-       .catch((err) => {
-         callback(err);
-       });
+        callback(null, res);
+        
+      })
+    })
+    .catch((err) => {
+      callback(err);
+    });
 
   }
-  }
+}
